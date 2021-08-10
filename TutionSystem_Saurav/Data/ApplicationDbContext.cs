ad557213c879153081f7a9d39e947d5379c055cb
Saurav3629@gmail.com <Saurav3629@gmail.com>
2021-08-10 22:31:02 +0300

    Add project files.

---
+++ TutionSystem_Saurav/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TutionSystem_Saurav.Models;
+
+namespace TutionSystem_Saurav.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
